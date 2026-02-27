51c96e851c52fda5f1f1f2bd3e2c6427a09b286f
LOC-DEV71 <loclc8533@ut.edu.vn>
2026-02-27 11:49:14 +0700

    update connect socket

--- src/socket/index.socket.jsx
+++ src/socket/index.socket.jsx
@@ -5,9 +5,12 @@ import { io } from "socket.io-client";
 //   autoConnect: false
 // });
 
+
 const socket = io(import.meta.env.VITE_API_URL, {
+  transports: ["websocket"],
   withCredentials: true
 });
+
 
 // const socket = io("https://nodejs-production-a6f1.up.railway.app/", {
 //   withCredentials: true,
